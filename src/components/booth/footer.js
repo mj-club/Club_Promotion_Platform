86f816f71238137528b847f89020c4cef2da7803
Dongjun Jeong <dkdlel1087@naver.com>
2021-03-06 10:17:49 +0900

    complete2

--- src/components/booth/footer.js
+++ src/components/booth/footer.js
@@ -1,49 +1,62 @@
 import React, { useContext } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-scroll";
 import PortfolioContext from "../../context/context";
+import Title from "components/booth/title";
+import { Typography } from "@material-ui/core";
+function formatDate(date) {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+  return [year, month, day].join(".");
+}
 
+function Copyright() {
+  const date = new Date();
+  return (
+    <Typography variant="body2" style={{ color: "white" }}>
+      {"Copyright © "}
+      <Link
+        style={{ color: "white" }}
+        href="https://github.com/mj-club/Club_Promotion_Platform"
+      >
+        총동아리연합회 비상대책위원회
+      </Link>{" "}
+      {/* {` ${formatDate(date)}`} */}
+    </Typography>
+  );
+}
 const Footer = () => {
   // const { footer } = useContext(PortfolioContext);
   // const { networks } = footer;
 
   return (
-    <footer className="footer navbar-static-bottom" >
+    <footer className="footer navbar-static-bottom">
       <Container>
-        <span className="back-to-top">
-          <Link to="hero" smooth duration={1000}>
-            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
-          </Link>
-        </span>
-        <div className="social-links">
-          {/* {networks &&
-            networks.map((network) => {
-              const { id, name, url } = network;
-              return (
-                <a
-                  key={id}
-                  href={url || "https://github.com/cobidev/gatsby-simplefolio"}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  aria-label={name}
-                >
-                  <i className={`fa fa-${name || "refresh"} fa-inverse`} />
-                </a>
-              );
-            })} */}
-        </div>
         <hr />
-        <p className="footer__text" style={{display: "flex", justifyContent:"center"}}>
-          {/* © {new Date().getFullYear()} - Template developed by{" "}
+        <Row className="justify-content-md-center">
+          <p>명지대학교 인문캠퍼스 총동아리연합회 비상대책위원회</p>
+        </Row>
+        <Row className="justify-content-md-center">
+          <p
+            className="footer__text"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            {/* © {new Date().getFullYear()} - Template developed by{" "}
           <a
             href="https://github.com/cobidev"
             target="_blank"
             rel="noopener noreferrer"
           > */}
-          명지대학교 인문캠퍼스 총동아리연합회 <br></br>
-          비대상대책위원장 양성훈
-          {/* </a> */}
-        </p>
+            Developers: 비대상대책위원장 양성훈 | 기획재정국장 정동준 |
+            복지정책국장 김휘준 <br></br>
+            Desinger: 대외홍보국장 노현정<br></br>
+            Copyright © 명지대학교 인문캠퍼스 총동아리연합회 비상대책위원회
+          </p>
+        </Row>
       </Container>
     </footer>
   );
